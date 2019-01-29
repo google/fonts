c68ced72da49dca6b289760a5f262d9a983ba5fb
Marc Foley <m.foley.88@gmail.com>
2019-01-29 11:12:48 +0000

    Run CI on PRs which have modified more than one family

--- viz_pr.py
+++ viz_pr.py
@@ -8,7 +8,6 @@ import subprocess
 from uuid import uuid4
 
 
-REPORT_DIR = 'out'
 GFR_URL = 'http://159.65.243.73'
 
 
@@ -25,20 +24,27 @@ def post_media_to_gfr(paths, uuid):
     return [os.path.join(GFR_URL, i) for i in r.json()['items']]
 
 
-def post_gh_msg(msg):
-    r = requests.post("https://api.github.com/repos/{}/issues/{}/comments".format(os.environ['TRAVIS_REPO_SLUG'], os.environ['TRAVIS_PULL_REQUEST']),
+def post_gh_msg(msg, repo_slug=None, pull_id=None):
+    pull_id = pull_id if pull_id else os.environ['TRAVIS_PULL_REQUEST']
+    repo_slug = repo_slug if repo_slug else os.environ['TRAVIS_REPO_SLUG']
+    r = requests.post("https://api.github.com/repos/{}/issues/{}/comments".format(repo_slug, pull_id),
         data=json.dumps({'body': msg}),
         headers={'Authorization': 'token {}'.format(os.environ['GH_TOKEN'])})
     print(r.text)
 
 
-def get_fonts_in_pr():
+def get_fonts_in_pr(repo_slug=None, pull_id=None):
     font_paths = []
-    r = requests.get("https://api.github.com/repos/{}/pulls/{}/files".format(os.environ['TRAVIS_REPO_SLUG'], os.environ['TRAVIS_PULL_REQUEST']),
+    pull_id = pull_id if pull_id else os.environ['TRAVIS_PULL_REQUEST']
+    repo_slug = repo_slug if repo_slug else os.environ['TRAVIS_REPO_SLUG']
+    r = requests.get("https://api.github.com/repos/{}/pulls/{}/files".format(repo_slug, str(pull_id)),
         headers={'Authorization': 'token {}'.format(os.environ['GH_TOKEN'])})
     for item in r.json():
-        if item['filename'].endswith('.ttf'):
-            font_paths.append(item['filename'])
+        filename = item['filename']
+        if filename.endswith('.ttf'):
+            if len(os.path.normpath(filename).split(os.path.sep)) > 3:
+                continue
+            font_paths.append(filename)
     return font_paths
 
 
@@ -53,17 +59,33 @@ def find_files(directory, ext='.gif'):
     return found_files
 
 
+def sep_fonts_into_families(fonts):
+    families= {}
+    for font in fonts:
+        family = os.path.basename(font).split("-")[0]
+        if family not in families:
+            families[family] = []
+        families[family].append(font)
+    return families
+
+
 def main():
     fonts_after = get_fonts_in_pr()
-    subprocess.call(["gftools", "qa"] + \
-                    fonts_after + \
-                    ["-a", "-o", REPORT_DIR])
-    report_zip = shutil.make_archive("out", 'zip', REPORT_DIR)
-    uuid = str(uuid4())
-    zip_url = post_media_to_gfr([report_zip], uuid)
-    msg = "Diff images: {}".format(zip_url[0])
-    post_gh_msg(msg)
+    families = sep_fonts_into_families(fonts_after)
+    for family, fonts in families.items():
+        family_qa_dir = "{}_qa".format(family)
+        subprocess.call(["gftools", "qa"] + \
+                        fonts + \
+                        ["-a", "-o", family_qa_dir])
+        # upload QA zip to GFRegression
+        report_zip = shutil.make_archive(family_qa_dir, "zip", family_qa_dir)
+        uuid = str(uuid4())
+        zip_url = post_media_to_gfr([report_zip], uuid)
+        msg = "{} Diff images: [{}.zip]({})".format(family, family_qa_dir, zip_url[0])
+        # TODO (M Foley as FB report and diff report)
+        post_gh_msg(msg)
 
 
 if __name__ == '__main__':
     main()
+

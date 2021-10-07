011b5d4cf49d1f9cdcac5b6cc9f709ad3b4e7320
Yanone <post@yanone.de>
2021-10-07 12:07:42 +0200

    Create issues.sh

---
+++ issues.sh
@@ -0,0 +1,1 @@
+gh issue create --title "Upgrade Baloo Da 2 (Bengali)" --body "https://github.com/EkType/Baloo2-Variable" -a "yanone" -p "Yanone's roadmap" -m "2021 Q4" -l "- Ready for Review" -l "II Font Upgrade" -l "III VF Replacement"

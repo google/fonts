26dfb5aade38e6c1304b371c66a1281b02cd0920
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2022-03-04 17:30:55 -0300

    Correct Google companyname

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 #
 # Copyright 2022 The Google Fonts Tools Authors.
-# Copyright 2017,2022 Google Inc. All Rights Reserved.
+# Copyright 2017,2022 Google LLC All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

81ba98e6b74172492aa5d213aa188f0e991385b5
Marc Foley <m.foley.88@gmail.com>
2022-07-01 11:48:37 +0100

    black

--- Lib/axisregistry/__init__.py
+++ Lib/axisregistry/__init__.py
@@ -324,7 +324,9 @@ def build_fvar_instances(ttFont, axis_dflts={}):
     stat_nameids = []
     if "STAT" in ttFont:
         if ttFont["STAT"].table.AxisValueCount > 0:
-            stat_nameids = [av.ValueNameID for av in ttFont["STAT"].table.AxisValueArray.AxisValue]
+            stat_nameids = [
+                av.ValueNameID for av in ttFont["STAT"].table.AxisValueArray.AxisValue
+            ]
 
     # rm old fvar subfamily and ps name records
     for inst in fvar.instances:

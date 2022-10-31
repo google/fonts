8d20486004311183f34d1917037369b4f7f0b7d1
Simon Cozens <simon@simon-cozens.org>
2022-10-31 14:09:31 +0000

    Report sample text category too

--- tests/test_dottedcircle.py
+++ tests/test_dottedcircle.py
@@ -20,11 +20,11 @@ def hb_font():
 @pytest.mark.parametrize("lang", langs.keys())
 def test_dotted_circle(lang, hb_font):
     item = langs[lang]
-    samples = [x for (_,x) in item.sample_text.ListFields()]
-    for sample in sorted(samples, key=lambda x:len(x)):
+    samples = item.sample_text.ListFields()
+    for sample_name, sample in sorted(samples, key=lambda x:len(x[1])):
         buf = hb.Buffer()
         buf.add_str(sample)
         buf.guess_segment_properties()
         hb.shape(hb_font, buf)
         ok = not any(info.codepoint == 0x25CC for info in buf.glyph_infos)
-        assert ok, f"Dotted circle found in {sample} ({lang})"
+        assert ok, f"Dotted circle found in {sample} ({lang}.{sample_name.name})"

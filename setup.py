694bd56db56cd88d6a29e139c8001dfd325edc56
Marc Foley <m.foley.88@gmail.com>
2022-11-10 17:01:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     install_requires=[
         # 3.7.0 fixed a bug on parsing some METADATA.pb files.
         # We cannot use v4 because our protobuf files have been compiled with v3.
-        'protobuf>=3.7.0, <=3.20.3',
+        'protobuf>=3.7.0, <4',
     ]
 )

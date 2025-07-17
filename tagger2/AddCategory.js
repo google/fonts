bdcefdf31c1cccdb1b99d9749087eb0b00ab7440
Marc Foley <m.foley.88@gmail.com>
2025-07-17 09:49:34 +0100

    Add missing file

---
+++ tagger2/AddCategory.js
@@ -0,0 +1,24 @@
+export default {
+    data: function() {
+        return {
+            category: "", // Input for new category
+        }
+    },
+    methods: {
+        addCategory() {
+            if (this.category.trim() === "") {
+                alert("Category name cannot be empty.");
+                return;
+            }
+            this.$emit('category-added', this.category.trim());
+            this.category = ""; // Reset the input field after adding
+        }
+    },
+    template: `
+        <div>
+            <h3>Add new category</h3>
+            <input type="text" v-model="category" placeholder="Category" />
+            <button @click="addCategory">Add</button>
+        </div>
+    `
+}

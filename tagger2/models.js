199e26ed77b6b8379d16024767271054b82993e0
Simon Cozens <simon@simon-cozens.org>
2025-07-17 09:48:03 +0100

    Support node ESM

--- tagger2/models.js
+++ tagger2/models.js
@@ -9,6 +9,12 @@ async function loadText(path) {
         const pathModule = require('path');
         const filePath = pathModule.join(__dirname, path);
         return fs.readFileSync(filePath, 'utf8')
+    } else if (typeof process !== 'undefined' ) {
+        // Node, but ESM module
+        const fs = await import('fs');
+        const pathModule = await import('path');
+        const filePath = pathModule.join(process.cwd(), path);
+        return fs.readFileSync(filePath, 'utf8');
     } else {
         throw new Error('Unknown environment');
     }
